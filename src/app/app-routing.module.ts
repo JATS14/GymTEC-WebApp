8a9732d889ff1c0f1d733a4379dc7bf30f3f7636
JATS14 <josetrejos2000@gmail.com>
2021-06-04 17:39:45 -0600

    Se asignan rooting y modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,48 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
+//Ventanas
+import { PrincipalComponent } from './PaginaPrincipal/principal.component';
+
+import { RegistroComponent } from './Registrarse/registro.component';
+
+import { VistaUsuario } from './VistaUsuario/vistaUsuario.component';
+import { Verclases } from './VistaUsuario/VerClases/verclases.component';
+
+import { VistaAdministrador } from './VistaAdministrador/vistaAdministrador.component';
+import { ConfigurarGym } from './VistaAdministrador/ConfigurarGym/configurarGym.component';
+import { GestionarEmpleados } from './VistaAdministrador/GestionEmpleados/gestionarEmpleados.component';
+import { GestionarInventario } from './VistaAdministrador/GestionInventario/gestionarInventario.component';
+import { GestionarProductos } from './VistaAdministrador/GestionProductos/gestionarProductos.component';
+import { GestionarPuestos } from './VistaAdministrador/GestionPuesto/gestionarPuestos.component';
+import { GestionarServicios } from './VistaAdministrador/GestionServicios/gestionarServicios.component';
+import { GestionarSucursal } from './VistaAdministrador/GestionSucursal/gestionarSucursal.component';
+import { GestionarTipoEquipo } from './VistaAdministrador/GestionTipoEquipo/gestionarTipoEquipo.component';
+import { GestionarTipoPlanilla } from './VistaAdministrador/GestionTipoPlantilla/gestionarTipoPlanilla.component';
+import { GestionarTratamiento } from './VistaAdministrador/GestionTratamientoSpa/gestionarTratamiento.component';
+
+
+
 export let routes: Routes = [
-  //{path: '', component: PrincipalComponent},
+  {path: '', component: PrincipalComponent},
+
+  {path: 'Registro', component: RegistroComponent},
+
+  {path: 'VistaUsuario', component: VistaUsuario},
+  {path: 'Verclases', component: Verclases},
+
+  {path: 'VistaAdministrador', component: VistaAdministrador},
+  {path: 'ConfigurarGym', component: ConfigurarGym},
+  {path: 'GestionarEmpleados', component: GestionarEmpleados},
+  {path: 'GestionarInventario', component: GestionarInventario},
+  {path: 'GestionarProductos', component: GestionarProductos},
+  {path: 'GestionarPuestos', component: GestionarPuestos},
+  {path: 'GestionarServicios', component: GestionarServicios},
+  {path: 'GestionarSucursal', component: GestionarSucursal},
+  {path: 'GestionarTipoEquipo', component: GestionarTipoEquipo},
+  {path: 'GestionarTipoPlanilla', component: GestionarTipoPlanilla},
+  {path: 'GestionarTratamiento', component: GestionarTratamiento},
+
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,50 @@ import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule, routes } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+//Ventanas
+import { PrincipalComponent } from './PaginaPrincipal/principal.component';
+
+import { RegistroComponent } from './Registrarse/registro.component';
+
+import { VistaUsuario } from './VistaUsuario/vistaUsuario.component';
+import { Verclases } from './VistaUsuario/VerClases/verclases.component';
+
+import { VistaAdministrador } from './VistaAdministrador/vistaAdministrador.component';
+import { ConfigurarGym } from './VistaAdministrador/ConfigurarGym/configurarGym.component';
+import { GestionarEmpleados } from './VistaAdministrador/GestionEmpleados/gestionarEmpleados.component';
+import { GestionarInventario } from './VistaAdministrador/GestionInventario/gestionarInventario.component';
+import { GestionarProductos } from './VistaAdministrador/GestionProductos/gestionarProductos.component';
+import { GestionarPuestos } from './VistaAdministrador/GestionPuesto/gestionarPuestos.component';
+import { GestionarServicios } from './VistaAdministrador/GestionServicios/gestionarServicios.component';
+import { GestionarSucursal } from './VistaAdministrador/GestionSucursal/gestionarSucursal.component';
+import { GestionarTipoEquipo } from './VistaAdministrador/GestionTipoEquipo/gestionarTipoEquipo.component';
+import { GestionarTipoPlanilla } from './VistaAdministrador/GestionTipoPlantilla/gestionarTipoPlanilla.component';
+import { GestionarTratamiento } from './VistaAdministrador/GestionTratamientoSpa/gestionarTratamiento.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    PrincipalComponent,
+
+    RegistroComponent,
+
+    VistaUsuario,
+    Verclases,
+
+    VistaAdministrador,
+    ConfigurarGym,
+    GestionarEmpleados,
+    GestionarInventario,
+    GestionarProductos,
+    GestionarPuestos,
+    GestionarServicios,
+    GestionarSucursal,
+    GestionarTipoEquipo,
+    GestionarTipoPlanilla,
+    GestionarTratamiento
+    
   ],
   imports: [
     BrowserModule,
